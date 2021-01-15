8ebc6078bb79f0e1d0cfc05c4e7145b941bf8d5d
AdrianKas <a.kasner@web.de>
2021-01-15 22:13:30 +0100

    Added the code for the like button

--- client.js
+++ client.js
@@ -1,1 +1,31 @@
-console.log("Holidays")
+var likeButtons = document.getElementsByClassName("btn btn-outline-warning btn-sm bi bi-suit-heart");
+
+
+for (var i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].isLiked = false;
+}
+
+for (var i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener("click", function () {
+        if (isLiked = false) {
+            likeButtons[i].style.backgroundColor = "#FFCF3E";
+            likeButtons[i].isLiked = true;
+        }
+        else {
+            likeButtons[i].style.backgroundColor = "white";
+            likeButtons[i].isLiked = false;
+        }
+    })
+}
+
+
+// likeButtons[2].addEventListener("click", function () {
+//     if (isLiked = false) {
+//         likeButtons[2].style.backgroundColor = "#FFCF3E";
+//         likeButtons[2].isLiked = true;
+//     }
+//     else {
+//         likeButtons[2].style.backgroundColor = "white";
+//         likeButtons[2].isLiked = false;
+//     }
+// })
