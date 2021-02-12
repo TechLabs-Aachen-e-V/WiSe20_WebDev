86918cbca34bdb6491a489d2c14930454d59f08c
SandhyaVasudevan7396 <sandhyav7396@gmail.com>
2021-02-12 15:41:38 +0100

    Read More button

--- client.js
+++ client.js
@@ -1,1 +1,15 @@
-console.log("Holidays")
+function readMore(button) {
+    let dots = document.querySelector(`.listDiscription[item="${button}"] .dots`);
+    let moreText = document.querySelector(`.listDiscription[item="${button}"] .more`);
+    let btnText = document.querySelector(`.listDiscription[item="${button}"] .myBtn`);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less";
+        moreText.style.display = "inline";
+    }
+}
