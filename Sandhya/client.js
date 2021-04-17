19f91f67b2a96fa5c68f719337de27ed522dcbd3
fatmaoza <fatma-oeztas@gmx.de>
2021-04-17 19:45:49 +0200

    Sandhya Changes

---
+++ Sandhya/client.js
@@ -0,0 +1,40 @@
+function readMore(button) {
+    let dots = document.querySelector(`.listDiscription[item="${button}"] .dots`);
+    let moreText = document.querySelector(`.listDiscription[item="${button}"] .more`);
+    let btnText = document.querySelector(`.listDiscription[item="${button}"] .myBtn`);
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.textContent = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.textContent = "Read less";
+        moreText.style.display = "inline";
+    }
+}
+
+const menuBtn = document.querySelector(".menu-icon span");
+const searchBtn = document.querySelector(".search-icon");
+const cancelBtn = document.querySelector(".cancel-icon");
+const items = document.querySelector(".nav-items");
+const form = document.querySelector("form");
+menuBtn.onclick = () => {
+    items.classList.add("active");
+    menuBtn.classList.add("hide");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
+cancelBtn.onclick = () => {
+    items.classList.remove("active");
+    menuBtn.classList.remove("hide");
+    searchBtn.classList.remove("hide");
+    cancelBtn.classList.remove("show");
+    form.classList.remove("active");
+    cancelBtn.style.color = "#ff3d00";
+}
+searchBtn.onclick = () => {
+    form.classList.add("active");
+    searchBtn.classList.add("hide");
+    cancelBtn.classList.add("show");
+}
