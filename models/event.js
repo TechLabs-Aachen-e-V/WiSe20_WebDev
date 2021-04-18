a75054a5a2fc0cb0a9b4517429eb4b058d6fa9a0
fatmaoza <fatma-oeztas@gmx.de>
2021-04-18 15:15:18 +0200

    commit zwischenergebnisse

--- models/event.js
+++ models/event.js
@@ -4,6 +4,7 @@ const Schema = mongoose.Schema;
 const EventSchema = new Schema({
     title: String,
     description: String,
+    category: String,
     date: String,
     location: String,
     file: Buffer
